8ef174a31378c8d6035e99f23c825cbd662003e5
Zhong Yinmin <zhongyinmin@pku.edu.cn>
2022-09-22 15:29:25 +0000

    update workload

--- benchmark/alpa/generate_workload.py
+++ benchmark/alpa/generate_workload.py
@@ -8,36 +8,38 @@ import pickle
 import time
 import threading
 
-class PossoinWorkLoad:
-    def __init__(self, model_num: int, tot_arrival_rate: float, proportions: List[float], duration: float, workload_name: str = "PossoinWorkLoad"):
+class WorkLoad:
+    """
+    Abstract class for DL Workload.
+    """
+    def __init__(self, model_num: int, duration: float, SLOs: List[float], workload_name: str):
         """
             @param model_num: the number of different models in this workload.
-            @param tot_arrival_rate: The total arrival rate of the requests for 
-                                     all the models, measured in Hz.
-            @param proportions: the proportion of the requests for each model 
-            @param duration: the duration of the workload, measured in Second.
+            @param duration: the duration of the workload, measured in second.
+            @param SLOs: the SLO for each model, measured in second.
         """
-        assert len(proportions) == model_num and tot_arrival_rate > 0 and sum(proportions) == 1
+        assert duration > 0
         self.model_num = model_num
-        self.tot_arrival_rate = tot_arrival_rate
-        self.proportions = proportions
         self.duration = duration
+        self.SLOs = SLOs
         self.workload_name = workload_name
 
-        self.model_ids = []
+        # attributes below are initialized by subclass
+        # model id for each request
+        self.model_ids = []   
+        # arrival time for each request
         self.arrive_times = []
-        rela_time = 0.0
-        while rela_time < duration:
-            self.model_ids.append(choice(model_num, p=proportions))
-            self.arrive_times.append(rela_time)
-            rela_time += exponential(1/tot_arrival_rate)
-        print(f"There are {len(self.arrive_times)} requests in total.")
-     
-    def plot(self, binwidth: int = 1, figname: str = None):
+
+    def plot(self, binwidth: float = 1, figname: str = None):
+        """
+        Plot the workload.
+            x-axis: arrival time
+            y-axis: number of arrival tasks per binwidth (in seconds)
+        """
         plt.figure()
         for id in range(self.model_num):
             arrive_times = [self.arrive_times[i] for i, model_id in enumerate(self.model_ids) if model_id == id]
-            arrive_hist, bin_edges = np.histogram(arrive_times, int((arrive_times[-1] - arrive_times[0])/binwidth))
+            arrive_hist, bin_edges = np.histogram(arrive_times, int(self.duration/binwidth))
             plt.plot(bin_edges[:-1], arrive_hist, label=f"model{id}")
         plt.title(self.workload_name)
         plt.xlabel("Time(s)")
@@ -48,50 +50,7 @@ class PossoinWorkLoad:
             plt.savefig(figname)
         else:
             plt.savefig(self.workload_name)
-    
-    def save(self, filename: str):
-        """
-        Save the workload to a csv file.
-            First row is metadata. (model_num, tot_arrival_rate, proportions, duration, SLOs, workload_name)
-            From second row to the end are requests info. (model_id, arrive_timestamp)
-        """
-        with open(filename, 'w', newline='') as workload_file:
-            writer = csv.writer(workload_file)
-            # metadata
-            writer.writerow([self.model_num, self.tot_arrival_rate, self.proportions, self.duration, self.SLOs, self.workload_name])
-            # request model id, arrival timestamp
-            for model_id, arrive_time in zip(self.model_ids, self.arrive_times):
-                writer.writerow([model_id, arrive_time])
-
-    @classmethod
-    def load(cls, filename: str):
-        """
-        Load the workload from a csv file.
-            First row is metadata. (model_num, tot_arrival_rate, proportions, duration, SLOs, workload_name)
-            From second row to the end are requests info. (model_id, arrive_timestamp)
-        """
-        with open(filename, 'r', newline='') as workload_file:
-            reader = csv.reader(workload_file)
-            # first row is metadata
-            model_num, tot_arrival_rate, proportions, duration, SLOs, workload_name = next(reader)
-            # request model id, arrival timestamp
-            model_ids, arrive_times = [], []
-            for model_id, arrive_time in reader:
-                model_ids.append(int(model_id))
-                arrive_times.append(float(arrive_time))
-            return cls(int(model_num), float(tot_arrival_rate), eval(proportions), float(duration), eval(SLOs), workload_name, model_ids, arrive_times)
-    
-    # def save(self, filename: str):
-    #     with open(filename, 'wb') as f:
-    #         pickle.dump(self, f)
-
-    # @classmethod
-    # def load(cls, filename: str):
-    #     with open(filename, 'rb') as f:
-    #         workload = pickle.load(f)
-    #     print(f"Load the {workload.workload_name} workload with {len(workload.model_ids)} requests.")
-    #     return workload
-
+ 
     def run(self, callbacks, timers, tolerance: float = 0.005):
         """
         Run the workload with the given callbacks.
@@ -103,12 +62,12 @@ class PossoinWorkLoad:
             timers(f"req{request_id}").stop()
 
         assert len(callbacks) == self.model_num
-        now = start = time.time()
         model_ids, arrive_times = deepcopy(self.model_ids), deepcopy(self.arrive_times)
         next_id, next_arrive_time = model_ids.pop(0), arrive_times.pop(0)
         request_times = [] # for tolerance check
         waiting_threads = []
         request_id = 0
+        now = start = time.time()
         # send requests
         while len(model_ids) > 0:
             if start + next_arrive_time <= now:
@@ -146,9 +105,72 @@ class PossoinWorkLoad:
             pickle.dump(latencies, f)
         return latencies
 
-def generate_workload(model_num: int, tot_arrival_rate: float, proportions: List[float], duration: float, workload_name: str = "PossoinWorkLoad"):
-    workload = PossoinWorkLoad(model_num, tot_arrival_rate, proportions, duration, workload_name)
-    workload.save(workload_name)
+
+class PossoinWorkLoad(WorkLoad):
+    def __init__(self, model_num: int, tot_arrival_rate: float, proportions: List[float], duration: float, 
+                       SLOs: List[float], workload_name: str = "PossoinWorkLoad", model_ids: List[int] = None, arrive_times: List[float] = None):
+        """
+            @param model_num: the number of different models in this workload.
+            @param tot_arrival_rate: The total arrival rate of the requests for 
+                                     all the models, measured in Hz.
+            @param proportions: the proportion of the requests for each model 
+            @param duration: the duration of the workload, measured in Second.
+            @param SLOs: the SLO for each model, measured in second.
+            @param model_ids: the model id for each request, if not given, generate during initialization.
+            @param arrive_times: the arrival time for each request, if not given, generate during initialization.
+        """
+        super().__init__(model_num, duration, SLOs, workload_name)
+        assert model_num == len(proportions) == len(SLOs) and sum(proportions) == 1 and tot_arrival_rate > 0
+        self.tot_arrival_rate = tot_arrival_rate
+        self.proportions = proportions
+
+        if model_ids is None:
+            assert arrive_times is None
+            rela_time = 0.0
+            while rela_time < duration:
+                self.model_ids.append(choice(model_num, p=proportions))
+                self.arrive_times.append(rela_time)
+                rela_time += exponential(1/tot_arrival_rate)
+        else:
+            self.model_ids = model_ids
+            self.arrive_times = arrive_times
+        print(f"There are {len(self.arrive_times)} requests in total.")
+
+    def save(self, filename: str):
+        """
+        Save the workload to a csv file.
+            First row is metadata. (model_num, tot_arrival_rate, proportions, duration, SLOs, workload_name)
+            From second row to the end are requests info. (model_id, arrive_timestamp)
+        """
+        with open(filename, 'w', newline='') as workload_file:
+            writer = csv.writer(workload_file)
+            # metadata
+            writer.writerow([self.model_num, self.tot_arrival_rate, self.proportions, self.duration, self.SLOs, self.workload_name])
+            # request model id, arrival timestamp
+            for model_id, arrive_time in zip(self.model_ids, self.arrive_times):
+                writer.writerow([model_id, arrive_time])
+
+    @classmethod
+    def load(cls, filename: str):
+        """
+        Load the workload from a csv file.
+            First row is metadata. (model_num, tot_arrival_rate, proportions, duration, SLOs, workload_name)
+            From second row to the end are requests info. (model_id, arrive_timestamp)
+        """
+        with open(filename, 'r', newline='') as workload_file:
+            reader = csv.reader(workload_file)
+            # first row is metadata
+            model_num, tot_arrival_rate, proportions, duration, SLOs, workload_name = next(reader)
+            # request model id, arrival timestamp
+            model_ids, arrive_times = [], []
+            for model_id, arrive_time in reader:
+                model_ids.append(int(model_id))
+                arrive_times.append(float(arrive_time))
+            return cls(int(model_num), float(tot_arrival_rate), eval(proportions), float(duration), eval(SLOs), workload_name, model_ids, arrive_times)
+    
+def generate_workload(model_num: int, tot_arrival_rate: float, proportions: List[float], duration: float, SLOs: List[float], workload_name: str = "PossoinWorkLoad"):
+    workload = PossoinWorkLoad(model_num, tot_arrival_rate, proportions, duration, SLOs, workload_name)
+    workload.save(f"{workload_name}")
     return workload
 
 def test_save_load():
@@ -164,7 +186,8 @@ def test_run():
     workload.run([lambda: x + 1]*2)
 
 if __name__ == "__main__":
-    even_workload = generate_workload(2, 10, [0.5, 0.5], 20, "Even_10Hz_20s")
-    even_workload.plot(0.5)
-    skew_workload = generate_workload(2, 10, [0.8, 0.2], 20, "Skewed8to2_10Hz_20s")
-    skew_workload.plot(0.5)
+    # even_workload = generate_workload(2, 10, [0.5, 0.5], 20, "Even_10Hz_20s")
+    # even_workload.plot(0.5)
+    # skew_workload = generate_workload(2, 10, [0.8, 0.2], 20, "Skewed8to2_10Hz_20s")
+    # skew_workload.plot(0.5)
+    skew_workload = generate_workload(2, 10, [0.8, 0.2], 20, [0.25, 0.25], "test_workload_8to2_10Hz_20s")
